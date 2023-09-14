f434e21f47e0d68bf8f1747c5bf5bf4684fa6247
James <jameshowie17@outlook.com>
2023-09-14 18:42:27 +0100

    added UploadLayout.tsx & SideNavMain.tsx

---
+++ app/layouts/UploadLayout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { usePathname } from "next/navigation";
+import TopNav from "./includes/TopNav";
+
+export default function UploadLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="bg-[#f8f8f8] h-[100vh]">
+        <TopNav />
+        <div className="flex justify-between mx-auto w-full px-2 max-w-[1140px]">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
